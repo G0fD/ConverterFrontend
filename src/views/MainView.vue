deb41898278dfd2922de4754d9743479d731b392
Jakub <gofdark231@gmail.com>
2024-02-27 15:38:30 +0100

    initial commit

---
+++ src/views/MainView.vue
@@ -0,0 +1,21 @@
+<script setup>
+import {onMounted, ref} from "vue";
+import {API_URL} from "@/common/constant";
+
+const currencies = ref([])
+
+onMounted(async ()=>{
+  await fetch(API_URL + "/getAll")
+})
+
+const response = await fetch(API_URL)
+
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
